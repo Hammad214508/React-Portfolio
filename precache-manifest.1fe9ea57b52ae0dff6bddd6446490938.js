87a5d729132589e4c55e1de346f3c16ff4863212
Hammad214508 <u1814453@live.warwick.ac.uk>
2022-11-20 21:58:08 +0000

    Updates

---
+++ precache-manifest.1fe9ea57b52ae0dff6bddd6446490938.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da8613794371b2fa18a70d03f5d4c2f",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "219d41322cf7a3fdccf8",
+    "url": "/React-Portfolio/static/css/main.2f3d1489.chunk.css"
+  },
+  {
+    "revision": "f7fe4f778c9a872f066a",
+    "url": "/React-Portfolio/static/js/2.230d4b5d.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/React-Portfolio/static/js/2.230d4b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219d41322cf7a3fdccf8",
+    "url": "/React-Portfolio/static/js/main.2490c2f4.chunk.js"
+  },
+  {
+    "revision": "51a8a88aaefd4a51c775",
+    "url": "/React-Portfolio/static/js/runtime-main.be668ddd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/React-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/React-Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/React-Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/React-Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/React-Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/React-Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/React-Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/React-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "074c759bcddffa259a562604f23085a0",
+    "url": "/React-Portfolio/static/media/bauddhikgeeks-transparent.074c759b.png"
+  },
+  {
+    "revision": "f2d490dc98d16b9d7939e8006d0debb7",
+    "url": "/React-Portfolio/static/media/bauddhikgeeks.f2d490dc.png"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/React-Portfolio/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/React-Portfolio/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "82473004d117a65c8ed30396c6299763",
+    "url": "/React-Portfolio/static/media/efi.82473004.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/React-Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/React-Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/React-Portfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/React-Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/React-Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/React-Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/React-Portfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/React-Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/React-Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/React-Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/React-Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/React-Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/React-Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/React-Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/React-Portfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/React-Portfolio/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "/React-Portfolio/static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/React-Portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/React-Portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "3d9465c3483dffb591b31f101699d0dc",
+    "url": "/React-Portfolio/static/media/gtu.3d9465c3.png"
+  },
+  {
+    "revision": "7a2dbf7a2595600c00118eea8a7a52f8",
+    "url": "/React-Portfolio/static/media/hackathon1.7a2dbf7a.svg"
+  },
+  {
+    "revision": "c601a953e228e94a08f9068d6416de9f",
+    "url": "/React-Portfolio/static/media/herschel_logo.c601a953.png"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "/React-Portfolio/static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "/React-Portfolio/static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "b75caeeec914380fe52a944528e843b6",
+    "url": "/React-Portfolio/static/media/leetcode.b75caeee.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "/React-Portfolio/static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "/React-Portfolio/static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/React-Portfolio/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "/React-Portfolio/static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/React-Portfolio/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "5dcd18d03068fbd9edeeb5003b21998a",
+    "url": "/React-Portfolio/static/media/nightowls.5dcd18d0.jpg"
+  },
+  {
+    "revision": "3e932ccce493d88daa54a33c2f4bec44",
+    "url": "/React-Portfolio/static/media/pic2.3e932ccc.png"
+  },
+  {
+    "revision": "579efebdf072f2ba2b8efd0ce7452485",
+    "url": "/React-Portfolio/static/media/pic3.579efebd.png"
+  },
+  {
+    "revision": "579efebdf072f2ba2b8efd0ce7452485",
+    "url": "/React-Portfolio/static/media/pic4.579efebd.png"
+  },
+  {
+    "revision": "b1d0dc281c67fd500e112020149671f9",
+    "url": "/React-Portfolio/static/media/pic5 2.b1d0dc28.png"
+  },
+  {
+    "revision": "b1d0dc281c67fd500e112020149671f9",
+    "url": "/React-Portfolio/static/media/pic5.b1d0dc28.png"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/React-Portfolio/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/React-Portfolio/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "7b7a76ef55658f6999ce00c5d55f85a5",
+    "url": "/React-Portfolio/static/media/skillenza.7b7a76ef.png"
+  },
+  {
+    "revision": "8901092d61bfd611f75368233b1774c6",
+    "url": "/React-Portfolio/static/media/ssec.8901092d.png"
+  },
+  {
+    "revision": "894ef0fc8dfe1c0ab658636b806049db",
+    "url": "/React-Portfolio/static/media/ssgandhy.894ef0fc.png"
+  },
+  {
+    "revision": "6447eef91976c4c970023693a5fd53e3",
+    "url": "/React-Portfolio/static/media/tirant_logo.6447eef9.png"
+  },
+  {
+    "revision": "246ad3ede6a55b0102afa9871cfcbf21",
+    "url": "/React-Portfolio/static/media/valora.246ad3ed.jpg"
+  },
+  {
+    "revision": "44ba03a453e1dbcfc370837c500fed9b",
+    "url": "/React-Portfolio/static/media/warwick_logo.44ba03a4.png"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "/React-Portfolio/static/media/wrighter.02fdae43.jpg"
+  }
+]);
